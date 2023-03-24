16620218374759f3fac42e45dee5e53b2de1d753
Alioth <EpsUMa@outlook.com>
2023-03-24 21:22:34 +0800

    fix: 🐛 disable image optimization
    the feature is not supported by netlify

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,11 +1,7 @@
 import Image from "next/image"
 
-const loader = ({src}) => {
-    return `${src}`
-}
-
 function VSCode() {
-  return <Image loader={loader} src="/favicons/vscode.png" alt="" width={28} height={28} />
+  return <Image src="/favicons/vscode.png" alt="" width={28} height={28} />
 }
 
 export { VSCode }
